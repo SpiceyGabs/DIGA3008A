651e562980669b3a3c511795efa7c51a4ad9b462
SpiceyGabs <sgsquared.2@gmail.com>
2025-04-24 00:45:09 +0200

    data types:variables, objects, arrays

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+//VARIABLES
+var  num1 = 50;  //globally define, access & change variables.
+var sentence = "This is a string";  //stores sentences and strings
+var integers = 50;
+var isEnabled = true //Boolean Values (true or false) without having to specify the data type
+
+//LET
+let sentence = "My Name is John"; 
+
+const constant = false;
+console.log(constant);   //shows constant variables when you inspect them on the site
+
+    //you can use java without these things
+
+    //Objects are variables that contain multiple data values, these = PROPERTIES
+    //objects use keys to name values
+    //Objects affect things in real time. 
+
+let car = {speed:300, model: "Dodge"};     //Object literal comment. "Key value pairs"
+console/log(car.model);
+
+    //built in objects that JavaScript builds on
+    randomNumber = Math.random();
+    console.log(randomNumber);
+
+    //DOM object 
+    window.alert("Hello I am JavaScript")           //Window is responsible for everything that happens inside of the browser
+//Alert is a JavaScript event that causes a popup
+    document.writeln(<h1>Hello I come from JavaScript</h1>   )                        //?????????????WHY DOES IT CANCEL OUT 'WRITE"AND REPLACE IT WITH "wRITEIN"
+
+
+    //ARRAYS with SQUARE BRACKETS []
+
+    let shopping = ["coffee", "milk" ]
+
